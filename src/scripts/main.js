531d6778902c2d6c9ad701eb274e0f5a78b13e97
Lucas <lucas.ezequiel82@gmail.com>
2024-08-13 21:19:51 -0300

    Criando a contagem regresiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
-console.log("olá mundo!")
+AOS.init();
+
+const dataDoevento = new Date("Dec 12, 2077 19:00:00");
+const timeStampDoEvento = dataDoevento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOvento % diaEmMs) / horaEmMs);
+    const MinutosAteOEvento = Math.floor((distanciaAteOvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOvento % minutoEmMs) / 1000)
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${MinutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if(distanciaAteOvento < 0) {
+        clearInterval(contaAsHoras)
+        document.getElementById('contador').innerHTML = 'Evento finalizou'
+    }
+}, 1000)
